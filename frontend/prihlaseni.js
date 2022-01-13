39f27daddeb5fe1a22ccf430b6fd267a1e3bd188
Jan Kozohorský <72701569+Zigecek@users.noreply.github.com>
2022-01-13 09:29:23 +0100

    Add files via upload

--- frontend/prihlaseni.js
+++ frontend/prihlaseni.js
@@ -6,8 +6,8 @@ location.search
     queryDict[item.split("=")[0]] = item.split("=")[1];
   });
 console.log(queryDict);
-if (queryDict.overeno) {
-  var el = document.getElementById("main");
-  var newline = `<h2 style="color: green;">Tvůj účet byl ověřen, můžeš se přihlásit!</h2>`;
+if (queryDict.overeno == "ano") {
+  var el = document.getElementById("SS");
+  var newline = `<h2 style="color: green;">Tvůj účet byl ověřen, můžeš se přihlásit!</h2>\n`;
   el.innerHTML = newline + el.innerHTML;
 }
