6005f249b090d485c57a8390c189db040aa012db
Jan Kozohorský <72701569+Zigecek@users.noreply.github.com>
2022-06-12 23:15:52 +0200

    Add files via upload

--- frontend/global.js
+++ frontend/global.js
@@ -1,13 +1,3 @@
-function time() {
-  document.getElementById("datum").innerText = new Date().toLocaleString(
-    "cs-CZ",
-    { timeZone: "CET" }
-  );
-}
-
-time();
-setInterval(time, 999);
-
 var username = null;
 var socket = io.connect("https://cykablyat.cz");
 var el = document.getElementById("loginBtn");
@@ -16,7 +6,6 @@ socket.on("connect", () => {
     socket.on("tokenVer", (usrnm) => {
       if (usrnm) {
         username = usrnm;
-        socket.emit("getMessages");
         el.innerHTML = `<p><span><a href="/ucet">${usrnm}</a></span> <span><a href="/odhlaseni">Odhlásit</a></span></p>`;
       } else {
         deleteAllCookies();
@@ -24,6 +13,7 @@ socket.on("connect", () => {
       }
     });
     socket.emit("verify", document.cookie.slice(13));
+    socket.emit("getMessages");
   } else {
     el.innerHTML = `<a href="/prihlaseni">Přihlásit</a>`;
   }
@@ -33,7 +23,23 @@ const deleteAllCookies = () => {
   const cookies = document.cookie.split(";");
   for (const cookie of cookies) {
     const eqPos = cookie.indexOf("=");
-    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
     document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
   }
 };
+
+var loadtime = Date.now();
+
+const cas = () => {
+  let newTime = new Date();
+  let dif = new Date(newTime.getTime() - loadtime);
+  document.getElementById("cas").innerText = dif.toLocaleTimeString("cs-CZ", {
+    timeZone: "UTC",
+  });
+  document.getElementById("cas2").innerText = newTime.toLocaleTimeString(
+    "cs-CZ",
+    { timeZone: "Europe/Prague" }
+  );
+};
+cas();
+setInterval(cas, 1000);
